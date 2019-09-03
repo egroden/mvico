7b971dc815826b87e3f0e86a726f25bc5b4e1549
Denis Egorov <denis.e@fora-soft.com>
2019-09-03 12:47:45 +0300

    Add Either

--- app/src/main/java/com/example/mvi/core/Data.kt
+++ app/src/main/java/com/example/mvi/core/Data.kt
@@ -1,6 +1,10 @@
 package com.example.mvi.core
 
-
 typealias Reducer<State, Action, Command> = (State, Action) -> Pair<State, Set<Command>>
 
 typealias Render<State> = (State) -> Unit
+
+sealed class Either<out L, out R> {
+    data class Left<T>(val value: T) : Either<T, Nothing>()
+    data class Right<T>(val value: T) : Either<Nothing, T>()
+}
