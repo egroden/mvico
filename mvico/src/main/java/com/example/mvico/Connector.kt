c496b46c9f477a62663d7f57e629d68d464cbd43
Denis Egorov <egorovdenis99@gmail.com>
2019-09-08 22:15:20 +0300

    Add Connector

---
+++ mvico/src/main/java/com/example/mvico/Connector.kt
@@ -0,0 +1,31 @@
+package com.example.mvico
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
+
+class Connector<Action, SideEffect, State, Subscription>(val feature: Feature<Action, SideEffect, State, Subscription>) {
+    val renderScope: CoroutineScope = MainScope()
+}
+
+fun <Action, SideEffect, State, Subscription> Connector<Action, SideEffect, State, Subscription>.bindAction(
+    action: Action
+) {
+    with(feature) {
+        featureScope.launch { actions.offer(action) }
+    }
+}
+
+fun <Action, SideEffect, State, Subscription> Connector<Action, SideEffect, State, Subscription>.connect(
+    render: Render<State>
+) {
+    renderScope.launch {
+        feature.states.consumeEach(render)
+    }
+}
+
+fun <Action, SideEffect, State, Subscription> Connector<Action, SideEffect, State, Subscription>.disconnect() {
+    renderScope.cancel()
+}
