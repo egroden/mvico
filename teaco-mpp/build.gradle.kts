56fb64f4e7dd80e2a2e5a84a89b19324c33562fc
indrih17 <indrih17@gmail.com>
2019-11-20 18:23:03 +0300

    Attempt to fix ios (6).

--- teaco-mpp/build.gradle.kts
+++ teaco-mpp/build.gradle.kts
@@ -6,66 +6,43 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.multiplatform")
     id("maven-publish")
+    id("dev.icerock.mobile.multiplatform")
 }
 
 kotlin {
-    android {
-        publishLibraryVariants("release")
-    }
-    iosArm64("ios")
-    iosX64("iosSim")
-
     sourceSets {
-        val coroutinesVersion = "1.3.2"
-        val lifecycleVersion = "2.2.0-rc02"
-
         all {
             languageSettings.apply {
                 useExperimentalAnnotation("kotlin.Experimental")
             }
         }
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:$coroutinesVersion")
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-        val androidMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib"))
-
-                implementation("androidx.appcompat:appcompat:1.1.0")
-                implementation("androidx.fragment:fragment-ktx:1.2.0-rc02")
-                implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
-                implementation("androidx.lifecycle:lifecycle-extensions:$lifecycleVersion")
-                implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:1.0.0-rc02")
-
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
-            }
-        }
-        val androidTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation(kotlin("test-junit"))
-            }
-        }
-        val iosMain by getting {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:$coroutinesVersion")
-            }
-        }
-        val iosTest by getting {
-        }
-        val iosSimMain by getting { dependsOn(iosMain) }
-        val iosSimTest by getting { dependsOn(iosTest) }
     }
+}
+
+dependencies {
+    val kotlinVersion: String by project
+    val coroutinesVersion: String by project
+    val lifecycleVersion: String by project
+
+    mppLibrary(
+        MultiPlatformLibrary(
+            common = "org.jetbrains.kotlin:kotlin-stdlib-common:$kotlinVersion",
+            android = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+        )
+    )
+    mppLibrary(
+        MultiPlatformLibrary(
+            common = "org.jetbrains.kotlinx:kotlinx-coroutines-core-common:$coroutinesVersion",
+            android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion",
+            ios = "org.jetbrains.kotlinx:kotlinx-coroutines-core-native:$coroutinesVersion"
+        )
+    )
+
+    androidLibrary(AndroidLibrary("androidx.appcompat:appcompat:1.1.0"))
+    androidLibrary(AndroidLibrary("androidx.fragment:fragment-ktx:1.2.0-rc02"))
+    androidLibrary(AndroidLibrary("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion"))
+    androidLibrary(AndroidLibrary("androidx.lifecycle:lifecycle-extensions:$lifecycleVersion"))
+    androidLibrary(AndroidLibrary("androidx.lifecycle:lifecycle-viewmodel-savedstate:1.0.0-rc02"))
 }
 
 tasks.withType<KotlinCompile> {
@@ -101,9 +78,7 @@ android {
 }
 
 // workaround for https://youtrack.jetbrains.com/issue/KT-27170
-configurations {
-    create("compileClasspath")
-}
+configurations.create("compileClasspath")
 
 task("iosTest") {
     dependsOn("linkDebugTestIos")
