e55fccdc8290b0051616243e1789ed66b1f66d0b
indrih17 <indrih17@gmail.com>
2019-11-28 17:03:05 +0300

    Version 0.5.1: * Updated implementation for swift compatibility.

--- teaco-mpp/src/iosMain/kotlin/com/egroden/teaco/IosConnector.kt
+++ teaco-mpp/src/iosMain/kotlin/com/egroden/teaco/IosConnector.kt
@@ -5,15 +5,11 @@ import kotlinx.coroutines.MainScope
 class IosConnector<Action, SideEffect, State, Subscription>(
     feature: Feature<Action, SideEffect, State, Subscription>
 ) {
-    val connector = Connector(MainScope(), feature)
+    private val connector = Connector(MainScope(), feature)
+
+    fun connect(render: Render<State, Subscription>) =
+        connector.connect(render)
+
+    fun bindAction(action: Action) =
+        connector bindAction action
 }
-
-fun <Action, SideEffect, State, Subscription> IosConnector<Action, SideEffect, State, Subscription>.connect(
-    render: Render<State, Subscription>
-) =
-    connector.connect(render)
-
-fun <Action, SideEffect, State, Subscription> IosConnector<Action, SideEffect, State, Subscription>.bindAction(
-    action: Action
-) =
-    connector bindAction action
